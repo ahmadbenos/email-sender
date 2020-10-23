4ea66de5934e16aa02a10d7018c6272e1195c93d
Ahmad <benos.tarabain@gmail.com>
2020-10-23 21:26:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const { urlencoded } = require("express");
+require("dotenv").config()
+
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
